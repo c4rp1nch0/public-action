875f094ae4ca1e17c8c1ef88df8fc85e5de9aebf
c4rp1nch0 <c4rp1nch0@protonmail.com>
2022-06-22 11:49:10 -0300

    debug

--- eslint-rd/entrypoint.sh
+++ eslint-rd/entrypoint.sh
@@ -47,7 +47,7 @@ pushd "${GITHUB_WORKSPACE}" >/dev/null \
     || (/bin/echo "Couldn't change the working directory to '${GITHUB_WORKSPACE}'" && exit 1)
 
 # Convert INPUT_ESL_PATH to an array of paths
-IFS=$INPUT_ESL_PATHS_SEPARATOR read -r -a TMP_ESLINT_PATHS <<< "${INPUT_ESL_PATHS[@]}" || \
+IFS="'${INPUT_ESL_PATHS_SEPARATOR}'" read -r -a TMP_ESLINT_PATHS <<< "${INPUT_ESL_PATHS[@]}" || \
     (/bin/echo "::error:: Couldn't create an array with the paths to scan" && exit 1)
 
 for p in "${TMP_ESLINT_PATHS[@]}"; do
